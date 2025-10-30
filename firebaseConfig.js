b5edfa33eb6060261e077fd93d721b9ac073dc68
Kranthi Vadisi <95272636+kranthi244@users.noreply.github.com>
2025-10-30 22:08:52 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeuC7hS30cJHXoTGx6BbmW8g_kwLGakDA",
+  authDomain: "vadisi-reports.firebaseapp.com",
+  projectId: "vadisi-reports",
+  storageBucket: "vadisi-reports.firebasestorage.app",
+  messagingSenderId: "574994089915",
+  appId: "1:574994089915:web:a83456675ac8f7c4fba69e"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
